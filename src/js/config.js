a35ebf2b8a89f3c4a15b5d5ac16726b0b0f96c69
Emmszhii <emmanuelmanalo707@gmail.com>
2022-07-23 07:16:56 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6c843b5a-572f-4d9f-bef2-0d82f4b219a1';
+export const MODAL_CLOSE_SEC = 2.5;
