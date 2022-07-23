8fea4aeac8052dff2fca331324fdc58bec19c576
Emmszhii <emmanuelmanalo707@gmail.com>
2022-07-23 10:50:53 +0800

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -120,10 +120,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -132,6 +128,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
